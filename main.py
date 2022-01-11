cb4a70c4d93fcfdb299b504867622ad7f34469aa
isMyResume <ismyresume@gmail.com>
2022-01-11 15:41:13 +0900

    feat: Add GUI

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from clipboard_to_base64_tag import clipboard_image_to_base64_tag
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QPushButton, QCheckBox, QGridLayout, QSizePolicy, QApplication
+
+from clipboard_to_base64_tag import get_clipboard_to_base64
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def clipboard_to_base64():
+    get_clipboard_to_base64()
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 200, 100)
+        self.copyButton = QPushButton("Copy")
+        self.copyButton.clicked.connect(clipboard_to_base64)
+        self.checkButton = QCheckBox("Always On")
+        self.checkButton.stateChanged.connect(self.alwaysontop)
+        layout = QGridLayout(self)
+        self.copyButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        layout.addWidget(self.copyButton, 1, 0, 1, 1)
+        layout.addWidget(self.checkButton, 2, 0, 1, 1)
+        self.setLayout(layout)
+
+    def alwaysontop(self, state):
+        if state == Qt.Checked:
+            self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+            self.show()
+        else:
+            self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowStaysOnTopHint)
+            self.show()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    clipboard_image_to_base64_tag()
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    app.exec_()
